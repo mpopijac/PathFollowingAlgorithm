0068ccfc2032bb27d3a22518daedff2c04dec87f
mpopijac <matija.popijac@hotmail.com>
2021-10-21 12:11:53 +0200

    Changed logger to slf4j

--- src/hr/mpopijac/Main.java
+++ src/hr/mpopijac/Main.java
@@ -1,22 +1,21 @@
 package hr.mpopijac;
 
 import hr.mpopijac.data.ResultData;
-import hr.mpopijac.exceptions.ReadFromFileException;
 import hr.mpopijac.services.PathFollowingAlgorithmService;
 import hr.mpopijac.util.FileUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class Main {
 
-    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws ReadFromFileException {
-        LOG.info(() -> "Application started with: " + args.length + " arguments.");
+    public static void main(String[] args) {
+        LOG.info("Application started with: {} arguments.", args.length);
         if (args.length == 1) {
-            LOG.info(() -> "Input file path:" + args[0]);
+            LOG.info("Input file path: {}", args[0]);
             try {
                 List<String> fileLines = FileUtil.readFromFile(args[0]);
                 printContentOfDocument(fileLines);
@@ -24,11 +23,10 @@ public class Main {
                 ResultData result = service.findPath(fileLines);
                 printSolution(result);
             } catch (Exception e) {
-                LOG.info("ERROR Message: " + e.getMessage());
-                LOG.info("ERROR StackTrace: " + Arrays.toString(e.getStackTrace()));
+                LOG.error("ERROR Message: " + e.getMessage(), e);
             }
         } else {
-            LOG.info("Please provide only one text document as a argument.");
+            LOG.info("Please provide only one path to the text document as an argument.");
         }
         LOG.info("Application STOP");
     }
